7ba6773729150c8c3dfb8859517406d535bb201f
SeungTae <dltmdxo1589@daum.net>
2025-10-11 15:15:37 +0900

    feat: Initial project setup

---
+++ src/test/java/com/viora/VioraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
