606b61c5c1b2a2edfe2cc40b2d2377bd09d7e6b9
SeungTae <dltmdxo1589@daum.net>
2025-10-12 01:34:30 +0900

    Review 글에 작성될 댓글 Entity

---
+++ src/main/java/com/viora/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.viora.entity;
+
+import com.viora.entity.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // '댓글(N)'은 '하나의 사용자(1)'에게 속합니다. (댓글 작성자)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // '댓글(N)'은 '하나의 리뷰(1)'에 속합니다. (댓글이 달린 리뷰)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(nullable = false)
+    private String text; // 댓글 내용
+
+    @Builder
+    public Comment(User user, Review review, String text) {
+        this.user = user;
+        this.review = review;
+        this.text = text;
+    }
+}
