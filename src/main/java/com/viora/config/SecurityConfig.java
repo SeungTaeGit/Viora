13f1644edccb631f1fdae54512367fd03729404d
SeungTae <dltmdxo1589@daum.net>
2025-10-14 00:40:02 +0900

    review delete 후 read 테스트 중 발생 한 오류(404가 아닌 401로 응답) 처리를 위한 코드 점검 및 수정

--- src/main/java/com/viora/config/SecurityConfig.java
+++ src/main/java/com/viora/config/SecurityConfig.java
@@ -29,29 +29,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // http 기본 인증 비활성화, REST API이므로 사용 안함
                 .httpBasic(config -> config.disable())
-                // csrf 보안 비활성화, REST API이므로 사용 안함
                 .csrf(config -> config.disable())
-                // 세션을 사용하지 않음 (JWT 방식이므로)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**", "/login/**", "/oauth2/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/reviews/**").permitAll() // 리뷰 조회는 누구나
+                        .requestMatchers(HttpMethod.GET, "/api/reviews/**").permitAll()
                         .anyRequest().authenticated()
                 )
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint(customAuthenticationEntryPoint)
                 )
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-
-                // OAuth2 로그인 설정 추가
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
-                                .userService(customOAuth2UserService) // 로그인 성공 후 사용자 정보를 처리할 서비스
+                                .userService(customOAuth2UserService)
                         )
-                        // 로그인 성공/실패 핸들러 추가 예정
                 );
 
         return http.build();
