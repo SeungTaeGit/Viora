11a298a13617a9d74483f4b88da79c86047ceaa3
SeungTae <dltmdxo1589@daum.net>
2025-10-16 18:05:22 +0900

    특정 리뷰 ID(reviewId)에 해당하는 댓글들을 페이지 단위로 조회하는 기능을 추가.

--- src/main/java/com/viora/repository/CommentRepository.java
+++ src/main/java/com/viora/repository/CommentRepository.java
@@ -4,6 +4,8 @@ import com.viora.entity.Comment;
 import com.viora.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -14,4 +16,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     // SELECT * FROM comments WHERE review_id = ?
     List<Comment> findAllByReview(Review review);
 
+    // 페이지네이션을 위한 메서드 추가
+    // Review Entity의 id 필드를 기준으로 Comment를 찾는다는 의미 (findBy + Review_Id)
+    // Spring Data JPA가 메서드 이름을 보고 SQL을 자동으로 생성
+    Page<Comment> findByReview_Id(Long reviewId, Pageable pageable);
 }
