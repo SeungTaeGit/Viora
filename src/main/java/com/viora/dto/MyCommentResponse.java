e1fbf016f56fbe9d93ed5a7e7d25ac541cda62fa
SeungTae <dltmdxo1589@daum.net>
2025-12-09 17:13:43 +0900

    닉네임 필드 추가.

--- src/main/java/com/viora/dto/MyCommentResponse.java
+++ src/main/java/com/viora/dto/MyCommentResponse.java
@@ -9,15 +9,17 @@ import java.time.LocalDateTime;
 public class MyCommentResponse {
     private final Long id;
     private final String text;
-    private final Long reviewId; // 댓글이 달린 리뷰의 ID
-    private final String reviewContentName; // 댓글이 달린 리뷰의 콘텐츠 이름
-    private final LocalDateTime createdAt; // 댓글 작성 시간
+    private final Long reviewId;
+    private final String reviewContentName;
+    private final LocalDateTime createdAt;
+    private final String authorNickname;
 
     public MyCommentResponse(Comment comment) {
         this.id = comment.getId();
         this.text = comment.getText();
-        this.reviewId = comment.getReview().getId(); // Comment 엔티티에서 Review 정보 가져오기
-        this.reviewContentName = comment.getReview().getContentName(); // Comment 엔티티에서 Review 정보 가져오기
+        this.reviewId = comment.getReview().getId();
+        this.reviewContentName = comment.getReview().getContentName();
         this.createdAt = comment.getCreatedAt();
+        this.authorNickname = comment.getUser().getNickname();
     }
 }
